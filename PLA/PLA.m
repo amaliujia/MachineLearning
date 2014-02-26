57eea6603b58f6bb8633445109342d4996cb13ff
Rui Wang <amaliujia@163.com>
2014-02-26 15:47:36 +0800

    Update PLA.m

--- PLA/PLA.m
+++ PLA/PLA.m
@@ -3,31 +3,31 @@ function count = PLA(w,combination,f2)
     count = 0; 
     temp = w * combination';
     [m,n] = size(temp);
-	   for i=1:n,
-   	  	temp(:,i) = mySignFun(temp(:,i));
-	   end
+    	for i=1:n,
+   	   temp(:,i) = mySignFun(temp(:,i));
+	end
     for i=1:n,
-   		if temp(:,i) ~= f2(i)
-   		     w = w + f2(i) * combination(i,:);
-		       count = count + 1;
-   		     flag = 1;
-  		     break;	
-	    end
+   	if temp(:,i) ~= f2(i)
+        w = w + f2(i) * combination(i,:);
+        count = count + 1;
+   	flag = 1;
+  	break;	
+	end
     end
     while( flag == 1)
            temp = w * combination';
     		[m,n] = size(temp);
    	    	 for i=1:n,
        			 temp(:,i) = mySignFun(temp(:,i));
-    		end
+    		 end
    		 for i=1:n,
-       		 if temp(:,i) ~= f2(i)
+       		    if temp(:,i) ~= f2(i)
            		w = w + f2(i) * combination(i,:);
-        	    count = count + 1;
-				flag = 1;
+        	    	count = count + 1;
+			flag = 1;
         		break;
-            else
-               flag = 0;
-            end  
-    	end
+            	   else
+               		flag = 0;
+            	   end  
+    		end
     end
